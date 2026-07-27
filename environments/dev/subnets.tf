6ba30320e32f719afc8ff632b6504007a72a4ff0
ashyT-Cloud <ashytech.innovate@gmail.com>
2026-07-27 10:53:34 +0000

    feat: complete production networking infrastructure

---
+++ environments/dev/subnets.tf
@@ -0,0 +1,72 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+
+# Public Subnet A
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.environment}-public-a"
+  }
+}
+
+# Public Subnet B
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.environment}-public-a"
+  }
+}
+
+# Private App A
+resource "aws_subnet" "private_app_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "${var.environment}-private-app-a"
+  }
+}
+
+# Private App B
+resource "aws_subnet" "private_app_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.12.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "${var.environment}-private-app-a"
+  }
+}
+
+# Private DB A
+resource "aws_subnet" "private_db_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.21.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "${var.environment}-private-db-a"
+  }
+}
+
+# Private DB B
+resource "aws_subnet" "private_db_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.22.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "${var.environment}-private-db-b"
+  }
+}
